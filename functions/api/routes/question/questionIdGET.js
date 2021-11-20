d621ff15d25a3edcf8380a41610e47b46168b490
변주현 <wngus4296@swu.ac.kr>
2021-11-21 06:00:50 +0900

    [FEAT] questionIdGET 구현

--- functions/api/routes/question/questionIdGET.js
+++ functions/api/routes/question/questionIdGET.js
@@ -2,24 +2,23 @@ const functions = require('firebase-functions');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
-const db = require('../../../db/db')
+const db = require('../../../db/db');
 const { questionDB } = require('../../../db');
 
 module.exports = async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
   let client;
-  
+
   try {
     client = await db.connect(req);
 
-    const allQuestion = await questionDB.getAllQuestion(client);
-    if(!allQuestion) return res.status(statusCode.BAD_REQUEST).sned(util.fail(statusCode.BAD_REQUEST,
-      responseMessage.NULL_VALUE))
-      
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_QUESTION_SUCCESS, allQuestion));
+    const question = await questionDB.getOneQuestion(client, id);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_QUESTION_SUCCESS, question));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
-    
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
