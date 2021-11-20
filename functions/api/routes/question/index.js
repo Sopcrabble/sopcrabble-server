4fae7061646db74f2d0335d060cf2a05b2b108bc
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-11-21 05:27:55 +0900

    [FEAT] questionGET 구현

--- functions/api/routes/question/index.js
+++ functions/api/routes/question/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', require('./questionPOST'));
+router.get('/', require('./questionGET'));
 
 module.exports = router;
---
+++ functions/api/routes/question/questionGET.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db')
+const { questionDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  let client;
+  
+  try {
+    client = await db.connect(req);
+
+    const allQuestion = await questionDB.getAllquestion(client);
+    if(!allQuestion) return res.status(statusCode.BAD_REQUEST).sned(util.fail(statusCode.BAD_REQUEST,
+      responseMessage.NULL_VALUE))
+      
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_QUESTION_SUCCESS, allQuestion));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
