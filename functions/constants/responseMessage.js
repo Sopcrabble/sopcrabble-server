648bef174799ce06f624ddefd7569689614db6c9
변주현 <wngus4296@swu.ac.kr>
2021-11-21 05:05:33 +0900

    [FEAT] answerListIdGET, answerIdGET 구현

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -2,39 +2,20 @@ module.exports = {
   NULL_VALUE: '필요한 값이 없습니다',
   OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
-  // 회원가입
-  CREATED_USER: '회원 가입 성공',
-  DELETE_USER: '회원 탈퇴 성공',
-  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+  // 서버 오류
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
+  DATABASE_ERROR: '서버 또는 DB로 인한 오류',
 
-  // 로그인
-  LOGIN_SUCCESS: '로그인 성공',
-  LOGIN_FAIL: '로그인 실패',
-  NO_USER: '존재하지 않는 회원입니다.',
-  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+  // 의뢰 생성 및 조회
+  CREATE_QUESTION_SUCCESS: '의뢰 생성 성공',
+  READ_ALL_QUESTION_SUCCESS: '모든 의뢰 조회 성공',
+  READ_ONE_QUESTION_SUCCESS: '의뢰명, 글자수 가져오기 성공',
+  READ_ONE_ANSWER_SUCCESS: '해당 의뢰 작성중인 단어 가져오기 성공',
+  UPDATE_ANSWER_SUCCESS: '입력한 글자 저장 성공',
+  READ_ALL_ANSWER_SUCCESS: '해당 의뢰 완성된 단어 가져오기 성공',
 
-  // 프로필 조회
-  READ_PROFILE_SUCCESS: '프로필 조회 성공',
+  // 좋아요
+  CREATE_LIKE_SUCCESS: '좋아요 누르기 성공',
 
-  // 유저
-  READ_ONE_USER_SUCCESS: '유저 조회 성공',
-  READ_ALL_USERS_SUCCESS: '모든 유저 조회 성공',
-  UPDATE_ONE_USER_SUCCESS: '유저 수정 성공',
-  DELETE_ONE_USER_SUCCESS: '유저 삭제 성공',
-
-  // 포스트
-  ADD_ONE_POST_SUCCESS: '포스트 추가 성공',
-  READ_ONE_POST_SUCCESS: '포스트 조회 성공',
-  READ_ALL_POSTS_SUCCESS: '모든 포스트 조회 성공',
-  UPDATE_ONE_POST_SUCCESS: '포스트 수정 성공',
-  DELETE_ONE_POST_SUCCESS: '포스트 삭제 성공',
-  NO_POST: '존재하지 않는 포스트입니다.',
-
-  // 서버 내 오류
-  INTERNAL_SERVER_ERROR: '서버 내 오류',
-
-  // 토큰
-  TOKEN_EXPIRED: '토큰이 만료되었습니다.',
-  TOKEN_INVALID: '토큰이 유효하지 않습니다.',
-  TOKEN_EMPTY: '토큰이 없습니다.',
+  READ_BEST_SUCCESS: '명예의 전당 가져오기 성공'
 };
